503e9d8437c96a1e51a0d85c42626c69a5e6bfcb
Jessica <jessicakjang@outlook.com>
2020-09-28 09:19:02 -0700

    image slideshow section

--- golfCourseSite/script.js
+++ golfCourseSite/script.js
@@ -1,8 +1,39 @@
+// open menu overlay
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
+// close menu overlay
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+// image slideshow 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
