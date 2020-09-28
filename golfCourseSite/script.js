03f2378acec90b116b092227a09904e1ab232d12
Jessica <jessicakjang@outlook.com>
2020-09-28 12:21:30 -0700

    js - title bar visual changes on scroll

--- golfCourseSite/script.js
+++ golfCourseSite/script.js
@@ -8,6 +8,33 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+// when user scrolls down, title section will resize
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        // change title bar background and padding
+        document.getElementById("titlebar").style.backgroundColor = "#fafafa";
+        document.getElementById("titlebar").style.padding = "0";
+        // change logo size
+        document.getElementById("logo").style.height = "70px";
+        // change title/h1 size and letter spacing
+        document.getElementById("title").style.fontSize = "2em";
+        document.getElementById("title").style.letterSpacing = "10px";
+
+    } else { 
+        // change back title bar background and padding on scroll up
+        document.getElementById("titlebar").style.backgroundColor = "transparent";
+        document.getElementById("titlebar").style.padding = "10px";
+        // change back logo size on scroll up
+        document.getElementById("logo").style.height = "125px";
+        // change back title/h1 size and letter spacing on scroll up
+        document.getElementById("title").style.fontSize= "3.5em";
+        document.getElementById("title").style.letterSpacing = "15px";
+        
+    }
+}
+
 // image slideshow 
 var slideIndex = 1;
 showSlides(slideIndex);
